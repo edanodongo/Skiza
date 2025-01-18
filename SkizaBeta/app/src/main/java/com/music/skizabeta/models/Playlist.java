1306c638f16b7e169459213cadf1b7ad7dd1d51a
Edan Odongo <edanodongo05@gmail.com>
2025-01-18 14:50:45 +0300

    Added Playlist model

---
+++ SkizaBeta/app/src/main/java/com/music/skizabeta/models/Playlist.java
@@ -0,0 +1,48 @@
+package com.music.skizabeta.models;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist implements Serializable {
+    private String id;
+    private String name;
+    private List<Track> tracks;
+
+    public Playlist(String id, String name, List<Track> tracks) {
+        this.id = id;
+        this.name = name;
+        this.tracks = tracks != null ? tracks : new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) {
+            tracks.add(track);
+        }
+    }
+
+    public void removeTrack(Track track) {
+        tracks.remove(track);
+    }
+}
