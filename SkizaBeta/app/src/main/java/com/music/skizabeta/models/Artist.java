540ea62e3b6a74b7da23ee9ba5e137872c4d95af
Edan Odongo <edanodongo05@gmail.com>
2025-01-18 14:47:45 +0300

    Added Artist model

---
+++ SkizaBeta/app/src/main/java/com/music/skizabeta/models/Artist.java
@@ -0,0 +1,27 @@
+package com.music.skizabeta.models;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Artist {
+    private String name;
+    private List<Album> albums;
+
+    public Artist(String name) {
+        this.name = name;
+        this.albums = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void addAlbum(Album album) {
+        albums.add(album);
+    }
+}
